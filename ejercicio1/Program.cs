6527c8b23f11424a486a503b5c78050dbcb4fd82
Victor2007-1 <n00480476@upn.pe>
2025-08-27 12:17:24 -0500

    Codigo agregado por victor

--- ejercicio1/Program.cs
+++ ejercicio1/Program.cs
@@ -15,6 +15,7 @@ namespace ejercicio1
             Console.WriteLine("grupo de fundamentos de algoritmos");
             Console.WriteLine("segunda clase de fundamntos de alogritmos");
             Console.WriteLine("Segunda semana de clases en la UPN");
+            Console.WriteLine("Hola soy tu papi");
         }
     }
 }
